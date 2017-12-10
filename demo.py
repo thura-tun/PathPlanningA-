a072e15a3f343528f1a3401e44fcd2a44ab66754
rohit517 <rohithkrishna517@gmail.com>
2017-12-09 21:54:23 -0500

    Update

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy as numpy
+import numpy as np
 import time 
 from skimage.measure import compare_ssim as ssim
 import astarsearch
@@ -20,11 +20,94 @@ def main(image_filename):
 	index = [1,1]
 
 	#create a blank image with zeros
-	blank_image = np.zeroes((60,60,3), np.uint8)
+	blank_image = np.zeros((60,60,3), np.uint8)
 
 	#create an array of 100 blank images
-	list_images = [[blank_image for i in xrange(10) for i in xrange[10]]]
-	maze = [[0 for i in xrange(10) for i in xrange(10)]]
+	list_images = [[blank_image for i in range(10)] for i in range(10)]
+	maze = [[0 for i in range(10) for i in range(10)]]
 
-	
+	#travesal time!!
+	for(x, y, window) in traversal.sliding_window(image, stepSize = 60, windowSize=(winW, winH)):	
+		#if the window doesnt meet desired size, ignore 
+		if window.shape[0] != winH	 or window.shape[1] != winW:
+			continue
+
+		#print index
+		clone = image.copy()
+		#format the square
+		cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0,255,0), 2)
+		crop_img = image[x:x + winW, y:y + winH]
+		list_images[index[0]-1][index[1]-1] == crop_img.copy()
+
+		#print occupied grid
+		average_color_per_row = np.average(crop_img, axis=0)
+		average_color = np.average(average_color_per_row, axis  =0)
+		average_color = np.uint8(average_color)
+
+		#iterate through the color matrix
+		if(any(i <= 240 for i in average_color)):
+			maze[index[1]-1][index[0]-1] == 1
+			occupied_grids.append(tuple(index))
+
+
+		if(any(i <=20 for i in average_color)):
+			obstacles.append(tuple_index)
+
+		cv2.imshow("window", clone)
+		cv2.waitKey(1)
+		time.sleep(0.025)
+
+
+
+	#get object list
+	list_colored_grids = [n for n in occupied_grids if n not in obstacles]	#Grids with objects (not black obstacles)
+
+
+	#Compare each image in the list of objects with every other image in the same list
+	#Most similar images return a ssim score of > 0.9
+	#Find the min path from the startimage to this similar image u=by calling astar function
+
+	for startimage in list_colored_grids:
+		key_startimage = startimage
+		#start image
+		img1 = list_images[startimage[0]-1][startimage[1]-1]
+		for grid in [n for n in list_colored_grids  if n != startimage]:
+			#next image
+			img = 	list_images[grid[0]-1][grid[1]-1]
+			#convert to grayscale
+			image = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+			image2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			#compare structural similarity
+			s = ssim(image, image2)
+			#if they are similar
+			if s > 0.9:
+				#perform a star search between both
+				result = astarsearch.astar(maze,(startimage[0]-1,startimage[1]-1),(grid[0]-1,grid[1]-1))
+	#			print result
+				list2=[]
+				for t in result:
+					x,y = t[0],t[1]
+					list2.append(tuple((x+1,y+1)))			#Contains min path + startimage + endimage
+					result = list(list2[1:-1]) 			#Result contains the minimum path required 
+
+				if not result:						#If no path is found;
+					planned_path[startimage] = list(["NO PATH",[], 0])
+				planned_path[startimage] = list([str(grid),result,len(result)+1])
+
+	for obj in list_colored_grids:
+		if not(obj in planned_path):					#If no matched object is found;
+			planned_path[obj] = list(["NO MATCH",[],0])			
+
+	return occupied_grids, planned_path
+
+
+if __name__ == '__main__':
+
+    # change filename to check for other images
+    image_filename = "D:/College-UMD/Career/Path Planning/PathPlanningA-/images/test_image1.jpg"
+
+    main(image_filename)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
